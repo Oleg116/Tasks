73b2b191fe6d018a32f9bb8c8fadaf936be2db76
Oleh Popovych <opopov5@softserveinc.com>
2021-06-24 17:44:30 +0300

    remove comments

--- figure_print/script.js
+++ figure_print/script.js
@@ -1,16 +1,3 @@
-// const figurePrint = size => {
-//     const star = document.createElement('p')
-//     const arr = [];
-//     for(let i = 0; i < 5; i++){
-//         arr.push('*')
-//         star.textContent =`${arr}`;
-//         document.getElementsByClassName('wrap-container')[0].appendChild(star)
-//     }
-// }
-
-// figurePrint(2)
-
-
 const figurePrint = event => {
     document.write('*')
     document.getElementsByTagName('body')[0].appendChild(document.createElement('br'))
